8afe48c3ffa8ca4257ca0bb7a4ed196175b05c72
Sina Moqadam <sina.moqadam@gmail.com>
2019-12-08 23:49:28 +0330

    refactoring the code

--- detect.py
+++ detect.py
@@ -1,22 +1,14 @@
 from Dataset.datasetReader import read_dataset
-from keras.models import Sequential
 from keras.models import Model
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from keras.layers import Embedding, Conv1D, concatenate, Input
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Embedding, Conv1D, concatenate, Input
 
-
-# train1, train2, train_label, test1, test2, test_label = read_dataset("Dataset/quora_duplicate_questions.tsv",
-#                                                                      train_sample_number=350000)
-
-
-model = Sequential()
 
 top_words = 20000
 embedding_vector_length = 32
 max_question_length = 100
 
-train1, train2, train_label, test1, test2, test_label = read_dataset("Dataset/tsv.tsv",
-                                                                     train_sample_number=150,
+train1, train2, train_label, test1, test2, test_label = read_dataset("Dataset/quora_duplicate_questions.tsv",
+                                                                     train_sample_number=350000,
                                                                      max_question_length=max_question_length)
 
 inputX1 = Input(shape=(max_question_length,))
@@ -31,9 +23,10 @@ x2 = Conv1D(filters=32, kernel_size=3, use_bias=True, activation="relu")(x2)
 x2 = Model(inputs=inputX2, outputs=x2)
 
 combined = concatenate([x1.output, x2.output])
-z = Dense(2, activation="relu")(combined)
-z = Dense(1, activation="sigmoid")(z)
 
+z = Dense(units=2, activation="relu")(combined)
+z = Flatten()(z)
+z = Dense(units=1, activation="sigmoid")(z)
 model = Model(inputs=[x1.input, x2.input], outputs=z)
 
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["binary_accuracy"])
